9d1c8ae882245bcef622d1b3db0a30cb2c9d19aa
Soumen Karmakar <soumen.karmakar@ericsson.com>
2024-07-23 21:01:41 +0100

    Initial commit

---
+++ finance-mgr-backend/src/main/java/com/soumen/app/finance_mgr_backend/FinanceMgrBackendApplication.java
@@ -0,0 +1,13 @@
+package com.soumen.app.finance_mgr_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceMgrBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceMgrBackendApplication.class, args);
+	}
+
+}
---
+++ finance-mgr-backend/src/test/java/com/soumen/app/finance_mgr_backend/FinanceMgrBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soumen.app.finance_mgr_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceMgrBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
